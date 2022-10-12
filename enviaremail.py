0f872266e81b78daaba2fc24b07c1c1a67d65190
Leoncc2 <leoncc2@hotmail.com>
2022-10-12 10:17:10 -0500

    msj

--- enviaremail.py
+++ enviaremail.py
@@ -4,9 +4,9 @@ import smtplib
 def enviar_email(email_destino,codigo):
     remitente = "jlcarrillo@uninorte.edu.co"
     destinatario = email_destino
-    mensaje = "Codigo de Activacion: "+ codigo
     mensaje = "<h2>Correo de Restablecimiento de Contraseñas</h2>"
     mensaje=mensaje+ "<a href='https://minticmensajeriaelectronica.herokuapp.com/restablecer/"+email_destino+"'>Click para Restablecer la contraseña</a>"
+    mensaje = "Codigo de Activacion: "+ codigo
     email = EmailMessage()
     email["From"] = remitente
     email["To"] = destinatario
